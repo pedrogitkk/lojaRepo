5de919bbf1eab4eb3196119cca74b12e4ce5e609
pedro.tristao@etec.sp.gov.br <pedrokk>
2025-10-16 12:15:47 -0300

    Criação do projeto e da classe Funcionario com atributos e construtor

---
+++ src/ClassePrincipal.java
@@ -0,0 +1,11 @@
+public class Funcionario {
+    float id;
+    String nome;
+    float salario;
+
+    public Funcionario(float id, String nome, float salario) {
+        this.id = id;
+        this.nome = nome;
+        this.salario = salario;
+    }
+}
